932583f77c06b5021116e447f704682e1632531d
Potato211 <Juanmf98@hotmail.com>
2018-09-08 18:23:12 -0300

    EF>

--- src/org/openxava/aulas/Aulas.java
+++ src/org/openxava/aulas/Aulas.java
@@ -1,16 +1,35 @@
 package org.openxava.aulas;
 
+import javax.persistence.*;
+
+import org.openxava.preceptoria.*;
+
 @javax.persistence.Entity 
-public class Aulas extends AulaTeoriaAulaTaller
-{
+
+public class Aulas{
 	
+	@Id @Column(length=40)
 	public String id;
 	
+	@Column(length=40)
 	public String ciclo;
 	
 	public Preceptoria preceptoria;
 
-	public Aulas(){
+	public String getid() {
+		return id;
+	}
+	
+	public void setid(String id) {
+		this.id = id;	
+	}
+	
+	public String getciclo() {
+		return ciclo;
+	}
+	
+	public void setciclo(String ciclo) {
+		this.ciclo = ciclo;	
 	}
 
 }
